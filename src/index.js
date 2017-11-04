c372f0391b37a641c066525ae74c1479c225e6b3
Dmitry Kutsenko <d2emonium@gmail.com>
2017-11-04 10:11:59 +0300

    No menu

--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
 const {app, Menu, BrowserWindow} = require('electron');
 
-const template = [
-  {
-    label: 'Edit',
-    submenu: [
-      {role: 'undo'},
-      {role: 'redo'}
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {role: 'reload'},
-      {role: 'forcereload'}
-    ]
-  },
-]
-
-if(process.platform === 'darwin'){
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      {role: 'about'},
-      {role: 'separator'}
-    ]
-  });
-  template[1].submenu.push(
-    {type: 'separator'},
-    {
-      label: 'Speech',
-      submenu: [
-        {role: 'startspeaking'},
-        {role: 'stopspeaking'}
-      ]
-    }
-  );
-}
-
-const menu = Menu.buildFromTemplate(template);
-Menu.setApplicationMenu(menu);
-
+/**
+ * const template = [
+ *  {
+ *    label: 'Edit',
+ *    submenu: [
+ *      {role: 'undo'},
+ *      {role: 'redo'}
+ *    ]
+ *  },
+ *  {
+ *    label: 'View',
+ *    submenu: [
+ *      {role: 'reload'},
+ *      {role: 'forcereload'}
+ *    ]
+ *  },
+ * ]
+ * 
+ * if(process.platform === 'darwin'){
+ *   template.unshift({
+ *     label: app.getName(),
+ *     submenu: [
+ *       {role: 'about'},
+ *       {role: 'separator'}
+ *     ]
+ *   });
+ *   template[1].submenu.push(
+ *     {type: 'separator'},
+ *     {
+ *       label: 'Speech',
+ *       submenu: [
+ *         {role: 'startspeaking'},
+ *         {role: 'stopspeaking'}
+ *       ]
+ *     }
+ *   );
+ * }
+ *
+ * const menu = Menu.buildFromTemplate(template);
+ * Menu.setApplicationMenu(menu);
+ */
 
 // electron.crashReporter.start();
 
